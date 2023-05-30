ce331bbda1cb590c685ff3ddb3261bb16e50954a
web3ten0 <web3ten0@gmail.com>
2023-05-30 15:38:08 +0900

    added rollup.config.mjs

---
+++ hoge/src/a.ts
@@ -0,0 +1,5 @@
+import b from "b";
+
+const a = 100;
+
+export const aAddB = (): number => a + b;
--- hoge/src/index.ts
+++ hoge/src/index.ts
@@ -1,8 +1,12 @@
+import { aAddB } from "a";
+
 export type Hoge = {
   title: string;
   amount: number;
 };
 
-export const hoge = (h: Hoge) => {
+export const hoge = (h: Hoge): number => {
   console.log(`title: ${h.title}, amount: ${h.amount}`);
+  const result = aAddB();
+  return result;
 };
